0c797968756a25c0d93ace7c8573da2f607ab4f8
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-03-20 22:46:46 -0400

    new

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/AppleCloneVitorLatorraca/",
   plugins: [react(), sentryVitePlugin({
     org: "jsm-x9",
     project: "javascript-react"
